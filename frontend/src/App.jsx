76f9cb736a0c062edbc5bdcb25c9efabc9281ea2
Bozai Cosmin <cosminbozai@outlook.com>
2023-02-09 20:31:26 +0000

    Frontend routes protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import useAuthContext from "./hooks/useAuthContext";
 import AddBook from "./pages/AddBook";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,14 +12,31 @@ import Register from "./pages/Register";
 import Showcase from "./pages/Showcase";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <Router>
       <Routes>
-        <Route path="/showcase" element={<Showcase />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/add-book" element={<AddBook />} />
+        <Route
+          path="/showcase"
+          element={!user ? <Showcase /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/showcase" />}
+        />
+        <Route
+          path="/add-book"
+          element={user ? <AddBook /> : <Navigate to="/showcase" />}
+        />
       </Routes>
     </Router>
   );
